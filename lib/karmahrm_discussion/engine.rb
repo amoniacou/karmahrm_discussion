e5555ce5f716bae6fceeffc5bca505a83bc31723
Stepan Boichyshyn <stepander007@gmail.com>
2017-05-05 16:23:49 +0300

    fixes

--- lib/karmahrm_discussion/engine.rb
+++ lib/karmahrm_discussion/engine.rb
@@ -1,37 +1,37 @@
 module KarmaHrmDiscussion
   class Engine < ::Rails::Engine
-    if defined?(ActsAsPluggable)
-      ::ActsAsPluggable::Plugin.register(
-        :karmahrm_discussion,
-        :collaboration, engine: self,
-                        description: 'Discussion Plugin',
-                        website: 'https://github.com/tachyons/karmahrm_discussion',
-                        author: 'Aboobacker MK',
-                        version: KarmaHrmDiscussion::VERSION,
-                        settings: {
-                          display_in_topbar: true
-                        },
-                        menu: {
-                          url: {
-                            controller: :posts,
-                            action: :index
-                          },
-                          class: 'fa fa-discussion',
-                          submenus: [
-                            {
-                              text: 'New',
-                              class: 'fa fa-new',
-                              url: { controller: :posts, action: :new }
-                            },
-                            {
-                              text: 'Index',
-                              class: 'fa fa-index',
-                              url: { controller: :posts, action: :index }
-                            }
-                          ],
-                          text: 'Discussions'
-                        })
-    end
+    # if defined?(ActsAsPluggable)
+    #   ::ActsAsPluggable::Plugin.register(
+    #     :karmahrm_discussion,
+    #     :collaboration, engine: self,
+    #                     description: 'Discussion Plugin',
+    #                     website: 'https://github.com/tachyons/karmahrm_discussion',
+    #                     author: 'Aboobacker MK',
+    #                     version: KarmaHrmDiscussion::VERSION,
+    #                     settings: {
+    #                       display_in_topbar: true
+    #                     },
+    #                     menu: {
+    #                       url: {
+    #                         controller: :posts,
+    #                         action: :index
+    #                       },
+    #                       class: 'fa fa-discussion',
+    #                       submenus: [
+    #                         {
+    #                           text: 'New',
+    #                           class: 'fa fa-new',
+    #                           url: { controller: :posts, action: :new }
+    #                         },
+    #                         {
+    #                           text: 'Index',
+    #                           class: 'fa fa-index',
+    #                           url: { controller: :posts, action: :index }
+    #                         }
+    #                       ],
+    #                       text: 'Discussions'
+    #                     })
+    # end
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths['db/migrate'].expanded.each do |expanded_path|
